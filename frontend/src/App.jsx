79ea8bafefeeae0476413679923dd5add6fe3efa
Atkinss1 <jordanatkins@live.com>
2024-01-29 19:53:07 -0700

    add: TopicList && TopicListItem components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import PhotoList from 'components/PhotoList';
-
-// const photos = new Array(3).fill().map((_, index) => ({ ...sampleDataForPhotoListItem, id: index + 1 }));
+import TopicList from 'components/TopicList';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      {/* {photos.map((photo) => (
-        <PhotoListItem key={photo.id} photo={photo} />
-        ))} */}
+      
+        <TopicList />
         <PhotoList />
     </div>
   );
